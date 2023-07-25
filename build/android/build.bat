7bba1a91be595ef44c401c411594debdaaf48aa6
bangbang.S <bangbang.song@gmail.com>
2023-07-25 19:58:56 +0800

    Update build.bat
    use different generator

--- build/android/build.bat
+++ build/android/build.bat
@@ -1,10 +1,18 @@
 setlocal
 
+if "%GITHUB_WORKFLOW%" == "" (set RUNNING_LOCALLY=1)
+
+if "%RUNNING_LOCALLY%" == "1" (
+    set GENERATOR="Visual Studio 16 2019"
+) else {
+    set GENERATOR="ninja"
+}
+
 rem https://github.com/google/filament/blob/main/android/Windows.md
 mkdir out\cmake-release
 cd out\cmake-release
 cmake ^
-    -G Ninja ^
+    -G %GENERATOR% ^
     -DCMAKE_INSTALL_PREFIX=..\release\filament ^
     -DFILAMENT_ENABLE_JAVA=NO ^
     -DCMAKE_BUILD_TYPE=Release ^
@@ -16,7 +24,7 @@ cd ..\..
 mkdir out\cmake-android-release-aarch64
 cd out\cmake-android-release-aarch64
 cmake ^
-    -G Ninja ^
+    -G %GENERATOR% ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_INSTALL_PREFIX=..\android-release\filament ^
     -DCMAKE_TOOLCHAIN_FILE=..\..\build\toolchain-aarch64-linux-android.cmake ^
@@ -27,7 +35,7 @@ cd ..\..
 mkdir out\cmake-android-release-arm7
 cd out\cmake-android-release-arm7
 cmake ^
-    -G Ninja ^
+    -G %GENERATOR% ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_INSTALL_PREFIX=..\android-release\filament ^
     -DCMAKE_TOOLCHAIN_FILE=..\..\build\toolchain-arm7-linux-android.cmake ^
@@ -38,7 +46,7 @@ cd ..\..
 mkdir out\cmake-android-release-x86_64
 cd out\cmake-android-release-x86_64
 cmake ^
-    -G Ninja ^
+    -G %GENERATOR% ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_INSTALL_PREFIX=..\android-release\filament ^
     -DCMAKE_TOOLCHAIN_FILE=..\..\build\toolchain-x86_64-linux-android.cmake ^
@@ -49,7 +57,7 @@ cd ..\..
 mkdir out\cmake-android-release-x86
 cd out\cmake-android-release-x86
 cmake ^
-    -G Ninja ^
+    -G %GENERATOR% ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_INSTALL_PREFIX=..\android-release\filament ^
     -DCMAKE_TOOLCHAIN_FILE=..\..\build\toolchain-x86-linux-android.cmake ^
